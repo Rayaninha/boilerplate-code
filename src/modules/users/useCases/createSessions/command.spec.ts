ed84b0f0c17c67f1ebf954db7f67e9a6f401269a
Darlysson Alves <darlysson.alves.dev@gmail.com>
2023-05-25 03:15:46 -0300

    tests: create sessions

---
+++ src/modules/users/useCases/createSessions/command.spec.ts
@@ -0,0 +1,58 @@
+import '../../../../shared/infra/http/env'
+
+import MongoDb from "../../../../shared/infra/database/mongoDb";
+import { CreateSessionsCommand } from "./command";
+import { CreateUsersCommand } from '../createUsers/command';
+
+let createSessionsCommand: CreateSessionsCommand;
+let createUsersCommand: CreateUsersCommand;
+
+describe('[COMMAND] - CREATE SESSIONS', () => {
+
+  beforeAll(async () => {
+    const db = await MongoDb.getDb();
+    createSessionsCommand = new CreateSessionsCommand(db);
+    createUsersCommand = new CreateUsersCommand(db);
+
+    await createUsersCommand.execute({
+      email: 'admin@example.com',
+      password: 'password',
+      role: 'toor',
+      name: 'admin'
+    })
+  })
+
+  afterAll(async () => {
+    const db = await MongoDb.getDb();
+    await db.dropDatabase()
+  })
+
+  test('should be able create a new session', async () => {
+    const result = await createSessionsCommand.execute({
+      email: 'admin@example.com',
+      password: 'password',
+    })
+
+    expect(result).toHaveProperty('user')
+    expect(result).toHaveProperty('token')
+    expect(result).toHaveProperty('refreshToken')
+  })
+
+  test('should be not able create a new session with invalid email', async () => {
+    const result = await createSessionsCommand.execute({
+      email: 'user@example.com',
+      password: 'password',
+    })
+
+    expect(result).toBe(false)
+  })
+
+  test('should be not able create a new session with invalid password', async () => {
+    const result = await createSessionsCommand.execute({
+      email: 'admin@example.com',
+      password: 'admin',
+    })
+
+    expect(result).toBe(false)
+  })
+})
---
+++ src/modules/users/useCases/createSessions/controller.spec.ts
@@ -0,0 +1,72 @@
+import { Application } from 'express';
+import '../../../../shared/infra/http/env';
+import { Db } from 'mongodb';
+import { App } from '../../../../shared/infra/http/app';
+import MongoDb, {
+  collections,
+} from '../../../../shared/infra/database/mongoDb';
+import request from 'supertest';
+import { hash } from 'bcryptjs';
+
+describe('[CONTROLLER] - CREATE SESSIONS', () => {
+  let app: Application;
+  let db: Db;
+
+  beforeAll(async () => {
+    app = await new App().setup();
+    db = await MongoDb.getDb();
+
+    const hashedPassword = await hash('admin', 8);
+
+    await db.collection(collections.users).insertOne({
+      name: 'admin',
+      email: 'admin@example.com',
+      password: hashedPassword,
+      role: 'toor',
+    });
+  });
+
+  afterAll(async () => {
+    await db.dropDatabase();
+  });
+
+  test('should be able create a new session', async () => {
+    const auth = await request(app)
+      .post('/users/auth')
+      .send({
+        email: 'admin@example.com',
+        password: 'admin',
+      });
+
+    expect(auth.body.r).toBe(true);
+    expect(auth.body.data).toHaveProperty('user');
+    expect(auth.body.data).toHaveProperty('token');
+    expect(auth.body.data).toHaveProperty('refreshToken');
+  });
+
+  test('should be not able create a new session with invalid email', async () => {
+    const auth = await request(app)
+      .post('/users/auth')
+      .send({
+        email: 'user@example.com',
+        password: 'admin',
+      });
+
+    
+    expect(auth.body.errors[0]).toBe('seus dados de entrada estão incorretos.')
+    expect(auth.body.r).toBe(false);
+  });
+
+  test('should be not able create a new session with invalid password', async () => {
+    const auth = await request(app)
+      .post('/users/auth')
+      .send({
+        email: 'admin@example.com',
+        password: 'user',
+      });
+
+    
+    expect(auth.body.errors[0]).toBe('seus dados de entrada estão incorretos.')
+    expect(auth.body.r).toBe(false);
+  });
+});
