12bf9f2698028637b73a6c6e00d76a5613016d53
Rayaninha <salesrayane11@gmail.con>
2023-08-17 11:16:59 -0300

    feat: add route getPet

--- src/modules/pets/infra/http/routes.ts
+++ src/modules/pets/infra/http/routes.ts
@@ -3,12 +3,15 @@ import { Db } from 'mongodb';
 import { EnsureAuthentication } from '../../../../shared/infra/http/middlewares/ensureAuthentication';
 import { Router } from 'express';
 import { SchemaValidator } from '../../../../shared/infra/http/middlewares/schemaValidator';
+import { GetPetController } from '../../../../modules/pets/useCases/getPets/controller';
 
 export class PetsRoutes {
   static getRoutes(db: Db): Router {
     const routes = Router();
 
     const { handle: createPets } = new CreatePetsController(db);
+    const { handle: getPet} = new GetPetController(db);
+
 
     // localhost:3000/pets/
     routes.post(
@@ -16,6 +19,14 @@ export class PetsRoutes {
       SchemaValidator(createPets.schema),
       EnsureAuthentication(createPets.auth),
       createPets.fn,
+    );
+    
+    // localhost:3000/pets/pet
+    routes.get(
+      '/pet',
+      SchemaValidator(getPet.schema),
+      EnsureAuthentication(getPet.auth),
+      getPet.fn,
     );
 
     return routes;
