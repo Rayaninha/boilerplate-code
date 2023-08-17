980ea137b9ced9a0d52d41fd5cab3873575a6986
Rayaninha <salesrayane11@gmail.con>
2023-08-17 11:16:42 -0300

    feat: add interface IFindPetByName

--- src/modules/pets/infra/helpers/types.ts
+++ src/modules/pets/infra/helpers/types.ts
@@ -1,3 +1,4 @@
+import { ObjectId } from 'mongodb';
 import { PetsEntities } from '../database/entities';
 
 export interface ICreatePets {
@@ -8,4 +9,9 @@ export interface ICreatePets {
 
 export interface IPetsRepositories {
   createPets(payload: ICreatePets): Promise<PetsEntities>;
+}
+
+export interface IFindPetByName {
+  name: string;
+  userId: string;
 }
